452c3d8c1dfc25d761d8edd0ae0d6d6898cad547
WangZesen <451324565@qq.com>
2018-02-14 13:30:41 +0100

    Add files via upload

--- part2/main.py
+++ part2/main.py
@@ -2,6 +2,7 @@ from data import *
 from sys import argv
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 assert len(argv) == 2
 
@@ -33,3 +34,60 @@ if argv[1] == '0': # Task 4.1
 	args = np.argsort(np.array(results))
 	for i in range(n):
 		print (names[args[i]])
+
+if argv[1] == "1":
+	n = 10
+	m = 10
+	maxIter = 200
+	maxDist = 2
+	node.dim = 2
+	
+	data = getData(1)
+	nodeMap = topology(mode = 1, n = m)
+	nodes = [node() for i in range(m)]
+	
+	for it in range(maxIter):
+		for i in range(n):
+			dists = np.array([nodes[j].dist(data[i]) for j in range(m)])
+			minNode = np.argmin(dists)
+			neighbours = nodeMap.neighbour(minNode)
+			curDist = round(maxDist - maxDist / (maxIter - 1) * it)
+			for j in neighbours:
+				nodes[j].update(data[i], curDist)	
+	
+	results = []
+	for i in range(n):
+		dists = np.array([nodes[j].dist(data[i]) for j in range(m)])
+		results.append(np.argmin(dists))
+	
+	x = []
+	y = []
+	args = np.argsort(np.array(results))
+	for i in range(n):
+		x.append(data[args[i]][0])
+		y.append(data[args[i]][1])
+	plt.plot(x, y)
+	plt.plot([x[0], x[-1]], [y[0], y[-1]])
+	plt.plot(x, y, 'o')
+	plt.show()
+
+if argv[2] == "2":
+	n = 349
+	m = 10
+	maxIter = 200
+	maxDist = 4
+	node.dim = 31
+	
+	data = getData(1)
+	nodeMap = topology(mode = 1, n = m)
+	nodes = [node() for i in range(m)]
+	
+	for it in range(maxIter):
+		for i in range(n):
+			dists = np.array([nodes[j].dist(data[i]) for j in range(m)])
+			minNode = np.argmin(dists)
+			neighbours = nodeMap.neighbour(minNode)
+			curDist = round(maxDist - maxDist / (maxIter - 1) * it)
+			for j in neighbours:
+				nodes[j].update(data[i], curDist)	
+	
