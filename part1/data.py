b26ee04ac29c9958c52113f6e68a2049878d2725
Bullet Zhang <crazybullet28zhang@gmail.com>
2018-02-14 18:56:40 +0100

    Update data.py

--- part1/data.py
+++ part1/data.py
@@ -1,19 +1,19 @@
 import math, random
 
-def generate(kind = 0, step = 0.1, st = 0, noise = 0):
-	x = []
-	y = []
-	if kind == 0:
-		while st < math.pi * 2:
-			x.append([st])
-			y.append(math.sin(st) + random.normalvariate(0, noise))
-			st = st + step
-	if kind == 1:
-		while st < math.pi * 2:
-			x.append([st])
-			if st < math.pi:
-				y.append(1 + random.normalvariate(0, noise))
-			else:
-				y.append(-1 +  + random.normalvariate(0, noise))
-			st = st + step
-	return x, y
+def generate(kind=0, step=0.1, st=0, noise=0):
+    x = []
+    y = []
+    if kind == 0:
+        while st < math.pi * 2:
+            x.append([st])
+            y.append(math.sin(2 * st) + random.normalvariate(0, noise))
+            st = st + step
+    if kind == 1:
+        while st < math.pi * 2:
+            x.append([st])
+            if math.sin(2 * st) + random.normalvariate(0, noise) > 0:
+                y.append(1)
+            else:
+                y.append(-1)
+            st = st + step
+    return x, y
